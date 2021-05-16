50bfd653c5cea64fc3b5c078fc2deb68eeb53989
Sebastian Thönissen <26723285+thoenissen@users.noreply.github.com>
2021-05-16 17:31:53 +0200

    - added raid command structure

---
+++ Scruffy.Commands/Raid/RaidCommandModule.cs
@@ -0,0 +1,181 @@
+﻿using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+using Scruffy.Commands.Base;
+using Scruffy.Services.Core;
+using Scruffy.Services.CoreData;
+using Scruffy.Services.Raid;
+
+namespace Scruffy.Commands.Raid
+{
+    /// <summary>
+    /// Raid commands
+    /// </summary>
+    [Group("raid")]
+#if RELEASE
+    [Hidden] // TODO
+#endif
+    public class RaidCommandModule : LocatedCommandModuleBase
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="localizationService">Localization service</param>
+        public RaidCommandModule(LocalizationService localizationService)
+            : base(localizationService)
+        {
+        }
+
+        #endregion // Constructor
+
+        #region Properties
+
+        /// <summary>
+        /// User management service
+        /// </summary>
+        public UserManagementService UserManagementService { get; set; }
+
+        /// <summary>
+        /// Message builder
+        /// </summary>
+        public RaidMessageBuilder MessageBuilder { get; set; }
+
+        #endregion // Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Starts the setup assistant
+        /// </summary>
+        /// <param name="commandContext">Current command context</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+        [Command("setup")]
+        public async Task Setup(CommandContext commandContext)
+        {
+            // TODO
+            await Task.Delay(1).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Joining an appointment
+        /// </summary>
+        /// <param name="commandContext">Current command context</param>
+        /// <param name="name">Name</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+        [Command("join")]
+        public async Task Join(CommandContext commandContext, string name)
+        {
+            // TODO
+            await Task.Delay(1).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Joining an appointment
+        /// </summary>
+        /// <param name="commandContext">Current command context</param>
+        /// <param name="name">Name</param>
+        /// <param name="role">Role</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+        [Command("join")]
+        public async Task Join(CommandContext commandContext, string name, string role)
+        {
+            // TODO
+            await Task.Delay(1).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Leaving an appointment
+        /// </summary>
+        /// <param name="commandContext">Current command context</param>
+        /// <param name="name">Name</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+        [Command("leave")]
+        public async Task Leave(CommandContext commandContext, string name)
+        {
+            // TODO
+            await Task.Delay(1).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Leaving an appointment
+        /// </summary>
+        /// <param name="commandContext">Current command context</param>
+        /// <param name="name">Name</param>
+        /// <param name="role">Role</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+        [Command("leave")]
+        public async Task Leave(CommandContext commandContext, string name, string role)
+        {
+            // TODO
+            await Task.Delay(1).ConfigureAwait(false);
+        }
+
+        #endregion // Methods
+
+        #region Roles
+
+        /// <summary>
+        /// Role administration
+        /// </summary>
+        [Group("roles")]
+        public class RaidRolesCommandModule : LocatedCommandModuleBase
+        {
+            #region Constructor
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="localizationService">Localization service</param>
+            public RaidRolesCommandModule(LocalizationService localizationService)
+                : base(localizationService)
+            {
+            }
+
+            #endregion // Constructor
+
+            #region Properties
+
+            /// <summary>
+            /// User management service
+            /// </summary>
+            public UserManagementService UserManagementService { get; set; }
+
+            /// <summary>
+            /// Message builder
+            /// </summary>
+            public RaidMessageBuilder MessageBuilder { get; set; }
+
+            #endregion // Properties
+
+            /// <summary>
+            /// Starting the personal roles assistant
+            /// </summary>
+            /// <param name="commandContext">Current command context</param>
+            /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+            [Command("own")]
+            public async Task RolesOwn(CommandContext commandContext)
+            {
+                // TODO
+                await Task.Delay(1).ConfigureAwait(false);
+            }
+
+            /// <summary>
+            /// Starting the roles assistant
+            /// </summary>
+            /// <param name="commandContext">Current command context</param>
+            /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+            [Command("all")]
+            public async Task RolesAll(CommandContext commandContext)
+            {
+                // TODO
+                await Task.Delay(1).ConfigureAwait(false);
+            }
+        }
+
+        #endregion // Roles
+    }
+}
---
+++ Scruffy.Services/Raid/RaidMessageBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+
+using DSharpPlus;
+
+using Scruffy.Services.Core;
+
+namespace Scruffy.Services.Raid
+{
+    /// <summary>
+    /// Building the lfg message
+    /// </summary>
+    public class RaidMessageBuilder : LocatedServiceBase
+    {
+        #region Fields
+
+        /// <summary>
+        /// Discord client
+        /// </summary>
+        private DiscordClient _client;
+
+        #endregion // Fields
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="client">Discord client</param>
+        /// <param name="localizationService">Localization service</param>
+        public RaidMessageBuilder(DiscordClient client, LocalizationService localizationService)
+            : base(localizationService)
+        {
+            _client = client;
+        }
+
+        #endregion // Constructor
+
+        #region Methods
+
+        /// <summary>
+        /// Refresh the message
+        /// </summary>
+        /// <param name="configurationId">Id of the configuration</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task RefreshMessageAsync(long configurationId)
+        {
+            // TODO
+            await Task.Delay(1).ConfigureAwait(false);
+        }
+
+        #endregion // Methods
+    }
+}
