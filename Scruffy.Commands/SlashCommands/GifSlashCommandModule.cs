280fc3875ca9bae83ac6d783341e20966d119623
MarkusGeorgi <99259903+JohnnyTheHotbody@users.noreply.github.com>
2022-04-23 16:04:30 +0200

    gif command migration

---
+++ Scruffy.Commands/SlashCommands/GifSlashCommandModule.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Interactions;
+
+using Scruffy.Services.Discord;
+using Scruffy.Services.Gifs;
+
+namespace Scruffy.Commands.SlashCommands;
+
+/// <summary>
+/// Gif commands
+/// </summary>
+[Group("gif", "Gif related commands")]
+public class GifSlashCommandModule : SlashCommandModuleBase
+{
+    #region Properties
+
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    public GifCommandHandler CommandHandler { get; set; }
+
+    #endregion // Properties
+
+    #region Command methods
+
+    /// <summary>
+    /// Creation of a one time reminder
+    /// </summary>
+    /// <param name="searchTerm">gif search term</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation</returns>
+    [SlashCommand("random", "Posting random GIFs")]
+    public Task Gif(string searchTerm) => CommandHandler.Gif(Context, searchTerm);
+
+    #endregion // Command methods
+}
