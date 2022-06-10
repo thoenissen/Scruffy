6c4d7129daefebfd081ae4852d90f32a5d328d60
thoenissen <26723285+thoenissen@users.noreply.github.com>
2022-06-10 15:38:23 +0200

    Database log backup

--- Scruffy.Services/Core/BackupJob.cs
+++ Scruffy.Services/Core/BackupJob.cs
@@ -22,7 +22,15 @@ public class BackupJob : LocatedAsyncJob
             if (await dbFactory.ExecuteSqlRawAsync($"BACKUP DATABASE [{Environment.GetEnvironmentVariable("SCRUFFY_DB_CATALOG")}] TO  DISK = N'{Environment.GetEnvironmentVariable("SCRUFFY_DB_BACKUP_DIRECTORY")}{Environment.GetEnvironmentVariable("SCRUFFY_DB_CATALOG")}_{DateTime.Now:yyyyMMdd}.bak' WITH NOFORMAT, NOINIT,  NAME = N'{Environment.GetEnvironmentVariable("SCRUFFY_DB_CATALOG")}-Full Database Backup', SKIP, NOREWIND, NOUNLOAD")
                                .ConfigureAwait(false) == null)
             {
-                LoggingService.AddJobLogEntry(LogEntryLevel.CriticalError, nameof(BackupJob), dbFactory.LastError.Message, null, dbFactory.LastError.ToString());
+                LoggingService.AddJobLogEntry(LogEntryLevel.CriticalError, nameof(BackupJob), "Database backup", dbFactory.LastError.Message, dbFactory.LastError.ToString());
+            }
+            else
+            {
+                if (await dbFactory.ExecuteSqlRawAsync($"BACKUP LOG [{Environment.GetEnvironmentVariable("SCRUFFY_DB_CATALOG")}] TO  DISK = N'{Environment.GetEnvironmentVariable("SCRUFFY_DB_BACKUP_DIRECTORY")}{Environment.GetEnvironmentVariable("SCRUFFY_DB_CATALOG")}_{DateTime.Now:yyyyMMdd}.trn' WITH NOFORMAT, NOINIT,  NAME = N'{Environment.GetEnvironmentVariable("SCRUFFY_DB_CATALOG")}-Full Database Backup', SKIP, NOREWIND, NOUNLOAD")
+                                   .ConfigureAwait(false) == null)
+                {
+                    LoggingService.AddJobLogEntry(LogEntryLevel.CriticalError, nameof(BackupJob), "Log backup", dbFactory.LastError.Message, dbFactory.LastError.ToString());
+                }
             }
         }
     }
