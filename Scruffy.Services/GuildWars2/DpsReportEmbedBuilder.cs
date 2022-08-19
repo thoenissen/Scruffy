4baea19908b365490a218fb82b1027eedb22440a
thoenissen <26723285+thoenissen@users.noreply.github.com>
2022-08-19 12:58:57 +0200

    Fixed guild raid date range in title

---
+++ Scruffy.Services/GuildWars2/DpsReportEmbedBuilder.cs
@@ -0,0 +1,54 @@
+using Discord;
+
+using Scruffy.Data.Enumerations.DpsReport;
+
+namespace Scruffy.Services.GuildWars2
+{
+    /// <summary>
+    /// Embed Builder extension for DPS reports
+    /// </summary>
+    public class DpsReportEmbedBuilder : EmbedBuilder
+    {
+        /// <summary>
+        /// Amount of report groups in the current row
+        /// </summary>
+        private int _reportGroupsInRow;
+
+        /// <summary>
+        /// Adds a sub title field
+        /// </summary>
+        /// <param name="title">The title to use</param>
+        public void AddSubTitle(string title)
+        {
+            AddField("\u200b", title);
+            _reportGroupsInRow = 0;
+        }
+
+        /// <summary>
+        /// Add a group of DPS reports
+        /// </summary>
+        /// <param name="group">The DPS report group</param>
+        /// <param name="content">The content of the report group</param>
+        public void AddReportGroup(DpsReportGroup group, string content)
+        {
+            AddReportGroup(group.AsText(), content);
+        }
+
+        /// <summary>
+        /// Add a group of DPS reports
+        /// </summary>
+        /// <param name="group">The DPS report group</param>
+        /// <param name="content">The content of the report group</param>
+        public void AddReportGroup(string group, string content)
+        {
+            if (_reportGroupsInRow > 1)
+            {
+                AddField("\u200b", "\u200b", false);
+                _reportGroupsInRow = 0;
+            }
+
+            AddField(group, content, true);
+            ++_reportGroupsInRow;
+        }
+    }
+}
--- Scruffy.Services/GuildWars2/LogCommandHandler.cs
+++ Scruffy.Services/GuildWars2/LogCommandHandler.cs
@@ -19,54 +19,6 @@ using Scruffy.Services.WebApi;
 namespace Scruffy.Services.GuildWars2;
 
 /// <summary>
-/// Embed Builder extension for DPS reports
-/// </summary>
-public class DpsReportEmbedBuilder : EmbedBuilder
-{
-    /// <summary>
-    /// Amount of report groups in the current row
-    /// </summary>
-    private int _reportGroupsInRow;
-
-    /// <summary>
-    /// Adds a sub title field
-    /// </summary>
-    /// <param name="title">The title to use</param>
-    public void AddSubTitle(string title)
-    {
-        AddField("\u200b", title);
-        _reportGroupsInRow = 0;
-    }
-
-    /// <summary>
-    /// Add a group of DPS reports
-    /// </summary>
-    /// <param name="group">The DPS report group</param>
-    /// <param name="content">The content of the report group</param>
-    public void AddReportGroup(DpsReportGroup group, string content)
-    {
-        AddReportGroup(group.AsText(), content);
-    }
-
-    /// <summary>
-    /// Add a group of DPS reports
-    /// </summary>
-    /// <param name="group">The DPS report group</param>
-    /// <param name="content">The content of the report group</param>
-    public void AddReportGroup(string group, string content)
-    {
-        if (_reportGroupsInRow > 1)
-        {
-            AddField("\u200b", "\u200b", false);
-            _reportGroupsInRow = 0;
-        }
-
-        AddField(group, content, true);
-        ++_reportGroupsInRow;
-    }
-}
-
-/// <summary>
 /// Handling log commands
 /// </summary>
 public class LogCommandHandler : LocatedServiceBase
@@ -428,7 +380,7 @@ public class LogCommandHandler : LocatedServiceBase
 
         if (bAnyUploads)
         {
-            var week = $"{startOfWeek:dd.} - {startOfWeek.AddDays(7):dd.MM.yy}";
+            var week = $"{startOfWeek:dd.} - {startOfWeek.AddDays(6):dd.MM.yy}";
             var title = Format.Bold(LocalizationGroup.GetFormattedText("DpsReportGuildRaidSummaryTitle", "Guild Raid {0}", week));
 
             embeds.Insert(0, BuildStats(knownGroups));
