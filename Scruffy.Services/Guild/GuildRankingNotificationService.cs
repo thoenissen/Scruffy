0818b77c1bf433f3d14fd60fb740e3f3befda142
thoenissen <26723285+thoenissen@users.noreply.github.com>
2022-08-26 16:11:34 +0200

    Rank notification unsubscribed the wrong event causing duplicate messages

--- Scruffy.Services/Guild/GuildRankingNotificationService.cs
+++ Scruffy.Services/Guild/GuildRankingNotificationService.cs
@@ -60,7 +60,7 @@ namespace Scruffy.Services.Guild
                 }
             }
 
-            _discordClient.LoggedIn -= OnConnected;
+            _discordClient.Connected -= OnConnected;
 
             return Task.CompletedTask;
         }
