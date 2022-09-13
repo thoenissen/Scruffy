9f047c3bcae13d57cf2446569b19fad394a20c8a
thoenissen <26723285+thoenissen@users.noreply.github.com>
2022-09-13 18:30:24 +0200

    Added looking for group tables

---
+++ Scruffy.Data/Entity/Queryable/LookingForGroup/LookingForGroupAppointmentQueryable.cs
@@ -0,0 +1,24 @@
+﻿using Scruffy.Data.Entity.Queryable.Base;
+using Scruffy.Data.Entity.Tables.LookingForGroup;
+
+namespace Scruffy.Data.Entity.Queryable.LookingForGroup
+{
+    /// <summary>
+    /// Queryable for accessing the <see cref="LookingForGroupAppointmentEntity"/>
+    /// </summary>
+    public class LookingForGroupAppointmentQueryable : QueryableBase<LookingForGroupAppointmentEntity>
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="queryable"><see cref="IQueryable"/>-object</param>
+        public LookingForGroupAppointmentQueryable(IQueryable<LookingForGroupAppointmentEntity> queryable)
+            : base(queryable)
+        {
+        }
+
+        #endregion // Constructor
+    }
+}
