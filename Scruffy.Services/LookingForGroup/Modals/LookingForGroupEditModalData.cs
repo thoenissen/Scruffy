6c7a7b1a831debcef7d80a178e093b4c95a0380d
thoenissen <26723285+thoenissen@users.noreply.github.com>
2022-09-13 21:27:29 +0200

    Added looking for group commands

---
+++ Scruffy.Services/LookingForGroup/Modals/LookingForGroupEditModalData.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace Scruffy.Services.LookingForGroup.Modals
+{
+    /// <summary>
+    /// Appointment edit
+    /// </summary>
+    public class LookingForGroupEditModalData : IModal
+    {
+        /// <summary>
+        /// Custom id
+        /// </summary>
+        public const string CustomId = "modal;lfg;edit";
+
+        /// <summary>
+        /// Creation of the appointment
+        /// </summary>
+        public string Title => "Appointment creation";
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        [InputLabel("Title")]
+        [RequiredInput]
+        [ModalTextInput(nameof(AppointmentTitle))]
+        public string AppointmentTitle { get; set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        [InputLabel("Description")]
+        [RequiredInput(false)]
+        [ModalTextInput(nameof(AppointmentDescription), TextInputStyle.Paragraph)]
+        public string AppointmentDescription { get; set; }
+    }
+}
