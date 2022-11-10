dcbf9d9a17f93b944a0586d196dde98eba648b3f
thoenissen <26723285+thoenissen@users.noreply.github.com>
2022-11-10 21:51:35 +0100

    Added DpsReportAnalyzer application

---
+++ Scruffy.ServiceHosts.DpsReportAnalyzer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Scruffy.ServiceHosts.DpsReportAnalyzer
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
